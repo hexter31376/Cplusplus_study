31c7621c0d831a2218a1c0fc07f8a6875dd201b3
WonyoungJang <zwonyoung33@gmail.com>
2024-07-15 00:42:48 +0900

    add part2

---
+++ P2_literal/S3_stringLeteral.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+using namespace std;
+
+// 문자는 '' 로 묶어서 문자열은 "" 로 묶어서 처리합니다.
+// 문과적 사고는 집어 던지십쇼, 인용 직접발언 그딴거 아닙니다.
+
+int main () {
+    cout << "문자는 이렇게 : " << '\101' << "\n";
+    cout << "문자열은 이렇게 : " << "Hello" << "\n";
+
+    return 0;
+}
